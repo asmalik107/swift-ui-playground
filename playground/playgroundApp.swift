ef0d9cfb1c463328b0d1c832adbdca3ab44b19aa
asmalik107 <asmalik@outlook.com>
2024-02-17 10:19:37 +0000

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by ASIM MALIK on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
